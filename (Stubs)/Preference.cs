856034f45af73488776116a56d585d056d872760
RenegadeMaster <dirk.coetsee@gmail.com>
2023-04-06 16:01:16 +0200

    initial solution as provided

---
+++ (Stubs)/Preference.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Fofx
+{
+    [Serializable]
+    public class Preference
+    {
+        public Preference(int id, int[] order)
+        {
+            Id = id;
+            Order = order;
+        }
+
+        public int Id { get; }
+        public int[] Order { get; }
+    }
+}
